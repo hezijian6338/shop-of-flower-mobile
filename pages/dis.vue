a8788427a8916c8c280703e22901fde0c2c73cd3
hezijian6338 <627222344@qq.com>
2020-04-03 16:25:13 +0800

    添加发现页面

---
+++ pages/dis.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="moment"></div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.moment {
+  margin: 0 auto;
+  width: 275px;
+  height: 148px;
+}
+</style>
